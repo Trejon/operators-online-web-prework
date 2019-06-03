ac5568554c8ba48190a85eb1a3e374244e02432c
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-03 01:58:53 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,16 @@
-require 'pry'
-binding.pry
 def unsafe?(speed)
+
   if speed < 40 || speed > 60
-    true
-  else 
-    false
+		true
+	else
+		false
+	end
 end
 
 
 
 def not_safe?(speed)
-	if speed < 40 || speed > 60 ? true : false 
-end
-	
 
+	speed < 40 || speed > 60 ? true : false
+end
 
